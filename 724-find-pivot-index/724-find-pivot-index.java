fb119a94f02cd72569fac1e7c0501b28347cd87b
Hashem Alhariry <45319166+HashemAlhariry@users.noreply.github.com>
2022-09-03 19:42:50 +0200

    Time: 2 ms (78.64%), Space: 52.2 MB (64.96%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+              int totalLeft=0;
+        int totalRight=0;
+
+        for (int i = 1; i <nums.length ; i++) {
+            totalRight+=nums[i];
+        }
+
+        if(totalLeft==totalRight)
+            return 0;
+
+
+        for (int i = 1; i <nums.length ; i++) {
+            totalRight-=nums[i];
+            totalLeft+=nums[i-1];
+            if(totalLeft==totalRight)
+                return i;
+        }
+
+        return -1;
+    }
+}
