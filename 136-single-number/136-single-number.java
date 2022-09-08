28a60f5e983f17eac45a302fce9c1626ba304c3d
Hashem Alhariry <45319166+HashemAlhariry@users.noreply.github.com>
2022-09-08 14:13:53 +0200

    Time: 1 ms (100.00%), Space: 42.4 MB (92.07%) - LeetHub

---
+++ 136-single-number/136-single-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+                   int number=nums[0];
+
+        for (int i = 1; i <nums.length ; i++) {
+            number=nums[i]^number;
+        }
+
+        return number;  
+    }
+}
