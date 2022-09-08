a928de4b041579e4bf21a853efe6a8db75f20c47
Hashem Alhariry <45319166+HashemAlhariry@users.noreply.github.com>
2022-09-08 14:08:39 +0200

    Time: 52 ms (5.09%), Space: 55.3 MB (38.32%) - LeetHub

---
+++ 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] sortArrayByParityII(int[] nums) {
+                     Queue<Integer> evenIndex =new PriorityQueue<>();
+        Queue<Integer> oddIndex=new PriorityQueue<>();
+        for (int i = 0; i < nums.length ; i++) {
+            if(nums[i]%2==0)
+                evenIndex.add(i);
+            else 
+                oddIndex.add(i);
+        }
+        int arr[]=new int[nums.length];
+        for (int i = 0; i < nums.length ; i++) {
+            if(i%2==0){
+                arr[i]= nums[evenIndex.poll()];
+            }else 
+                arr[i]=nums[oddIndex.poll()];
+        }
+        return arr;
+    }
+}
