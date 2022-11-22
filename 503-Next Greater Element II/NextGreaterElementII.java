4cce6064a8f9b8ad2fc13deeb416b15997640186
Hashem Alhariry <45319166+HashemAlhariry@users.noreply.github.com>
2022-11-22 11:00:32 +0200

    Add files via upload

---
+++ 503-Next Greater Element II/NextGreaterElementII.java
@@ -0,0 +1,81 @@
+//Problem
+/*
+Given a circular integer array nums (i.e., the next element of nums[nums.length - 1] is nums[0]), return the next greater number for every element in nums.
+
+The next greater number of a number x is the first greater number to its traversing-order next in the array, which means you could search circularly to find its next greater number. If it doesn't exist, return -1 for this number.
+
+
+
+Example 1:
+
+Input: nums = [1,2,1]
+Output: [2,-1,2]
+Explanation: The first 1's next greater number is 2;
+The number 2 can't find next greater number.
+The second 1's next greater number needs to search circularly, which is also 2.
+Example 2:
+
+Input: nums = [1,2,3,4,3]
+Output: [2,3,4,-1,4]
+
+
+Constraints:
+
+1 <= nums.length <= 104
+-109 <= nums[i] <= 109
+
+*/
+
+
+
+class NextGreaterElementII {
+
+    public static void main(String[] args) {
+        nextGreaterElements(new int[]{1,2,3,4,3});
+    }
+
+    public static int[] nextGreaterElements(int[] nums) {
+
+        int arr[]=new int[nums.length];
+
+        for (int i = 0; i <nums.length ; i++) {
+
+            int index=i;
+
+            if(index==nums.length-1)
+                index=0;
+            else
+                index++;
+
+            int tempNumber=nums[i];
+            while(index!=i ){
+
+                if(tempNumber<nums[index]) {
+                    tempNumber = nums[index];
+                    break;
+                }
+                index++;
+                if(index==nums.length)
+                    index=0;
+
+            }
+
+            if(tempNumber==nums[i])
+                arr[i]=-1;
+            else
+                arr[i]=tempNumber;
+        }
+
+        return arr;
+
+    }
+
+
+}
+
+
+
+
+
+
+
