6edb4c1978d6ad84cfac70710b65bf0ec5ca42e7
Hashem Alhariry <45319166+HashemAlhariry@users.noreply.github.com>
2023-01-09 17:58:26 +0200

    Time: 0 ms (100.00%), Space: 40 MB (67.77%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+ public  TreeNode invertTree(TreeNode root) {
+        TreeNode temp =root;
+        invTree(temp);
+        return root;
+    }
+
+    private  void  invTree(TreeNode root) {
+
+        if(root==null)
+            return ;
+
+            TreeNode tLeft = root.left;
+            TreeNode tRight=root.right;
+
+            if(tLeft!=null){
+                root.right=tLeft;
+            }else {
+                root.right=null;
+            }
+            if(tRight!=null) {
+                root.left = tRight;
+            }else {
+                root.left =null;
+            }
+            invertTree(root.left);
+            invertTree(root.right);
+    }
+}
