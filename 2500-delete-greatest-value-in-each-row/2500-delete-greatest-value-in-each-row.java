5d597404b11e25b20690b4325c284ff24b485c04
Hashem Alhariry <45319166+HashemAlhariry@users.noreply.github.com>
2023-01-27 19:02:43 +0200

    Time: 11 ms (34.53%), Space: 42.8 MB (55.36%) - LeetHub

--- 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.java
+++ 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.java
@@ -1,18 +1,26 @@
 class Solution {
     public int deleteGreatestValue(int[][] grid) {
-        int sum=0;
-        for(int i=0;i<grid.length;i++){
-            Arrays.sort(grid[i]);
-        }
-        for(int i=0;i<grid[0].length;i++){
-            int max=0;
-            for(int j=0;j<grid.length;j++){
-                if(max<grid[j][i]){
-                    max=grid[j][i];
-                }
+        int result=0;
+        List<PriorityQueue<Integer>> list = new ArrayList<>();
+
+        for (int i = 0; i < grid.length; i++) {
+            PriorityQueue<Integer> pQueue = new PriorityQueue<Integer>(Collections.reverseOrder());
+            for (int j = 0; j <grid[i].length ; j++) {
+                pQueue.add(grid[i][j]);
             }
-            sum+=max;
+            list.add(pQueue);
         }
-        return sum;
+        
+        for (int i = 0; i <grid[0].length ; i++) {
+            int localCounter=0;
+            for (int j = 0; j < list.size(); j++) {
+                int number =list.get(j).poll();
+                if(number>localCounter)
+                    localCounter=number;
+            }
+            result+=localCounter;
+        }
+        return result;
+
     }
 }
