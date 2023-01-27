f54179d013ec96e584210799d02aa7d31f75e42d
Hashem Alhariry <45319166+HashemAlhariry@users.noreply.github.com>
2023-01-27 21:39:23 +0200

    Time: 3 ms (67.25%), Space: 74.9 MB (28.95%) - LeetHub

--- 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.java
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.java
@@ -1,16 +1,22 @@
 class Solution {
     public int[][] sortTheStudents(int[][] score, int k) {
-              for (int i = 0; i < score.length; i++) {
-            for (int j = i+1; j <score.length ; j++) {
-                if(score[i][k]<score[j][k]){
-                    int tempScore [];
-                    tempScore=score[i];
-                    score[i]=score[j];
-                    score[j]=tempScore;
-                }
-            }
+
+        //Automatic sorting keys in ascending order
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+        int[][] result = new int[score.length][score[0].length];
+
+        for(int i =0 ; i<score.length ; i++){
+            //adding key = student grade , value = index of student
+            map.put(score[i][k] , i);
         }
-        return score;
- 
+
+        int index=score.length-1;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            //adding the array from the end to get the students sorted
+            result[index] = score[entry.getValue()];
+            index--;
+        }
+
+        return result;
     }
 }
