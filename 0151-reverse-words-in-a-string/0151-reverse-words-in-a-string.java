96b5b801ef4e2d78bcc8198fc8d42382b30591fc
Hashem Alhariry <45319166+HashemAlhariry@users.noreply.github.com>
2023-02-12 21:51:49 +0200

    Time: 4 ms (90.76%), Space: 42 MB (93.18%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,13 +1,48 @@
 class Solution {
- public static String reverseWords(String s) {
-        s=s.trim();
-        String [] words = s.split(" +");
-        StringBuilder sb = new StringBuilder();
-        int end = words.length - 1;
-        for(int i = 0; i<= end; i++){
-                sb.insert(0, words[i]);
-                if(i < end) sb.insert(0, " ");
+
+    public static String reverseWords(String s) {
+
+        char[] s1 = s.toCharArray();
+
+        String s2 = trimSpaces(s1, s1.length);
+        StringBuilder builder = new StringBuilder(s2);
+
+        strRev(builder, 0, builder.length() - 1);
+
+        int n = builder.length();
+        int start = 0, end = 0;
+
+        while (start < n) {
+
+            while (end < n && builder.charAt(end) != ' ')
+                ++end;
+
+            strRev(builder, start, end - 1);
+            start = end + 1;
+            ++end;
         }
-        return sb.toString();
+
+        return builder.toString();
+    }
+
+    public static void strRev(StringBuilder sb, int startRev, int endRev) {
+        while (startRev < endRev) {
+            char temp = sb.charAt(startRev);
+            sb.setCharAt(startRev++, sb.charAt(endRev));
+            sb.setCharAt(endRev--, temp);
+        }
+    }
+
+    static String trimSpaces(char[] a, int n) {
+        int x = 0, y = 0;
+
+        while (y < n) {
+            while (y < n && a[y] == ' ') y++;
+            while (y < n && a[y] != ' ') a[x++] = a[y++];
+            while (y < n && a[y] == ' ') y++;
+            if (y < n) a[x++] = ' ';
+        }
+
+        return new String(a).substring(0, x);
     }
 }
