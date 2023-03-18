69c3630808e33d8c3b5edbcb5b3955fe9e0f55b3
Hashem Alhariry <45319166+HashemAlhariry@users.noreply.github.com>
2023-03-18 18:22:57 +0200

    Time: 1 ms (77.93%), Space: 42.9 MB (5.07%) - LeetHub

---
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        if(root==null)
+            return new ArrayList<>();
+
+        List<List<Integer>> allLevels = new ArrayList<>();
+        Queue<TreeNode> queue= new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> currentLevel= new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode temp = queue.poll();
+
+                if(temp.left!=null)
+                    queue.add(temp.left);
+                if(temp.right!=null)
+                    queue.add(temp.right);
+
+                currentLevel.add(temp.val);
+            }
+            allLevels.add(currentLevel);
+        }
+
+        for (int i = 0; i < allLevels.size(); i++) {
+            if(i%2!=0)
+                Collections.reverse(allLevels.get(i));
+        }
+        return allLevels;
+    }
+}
