6c0acaab11af277840d6b804080a87936b57068e
Hashem Alhariry <45319166+HashemAlhariry@users.noreply.github.com>
2023-09-21 00:58:34 +0300

    Time: 27 ms (98.93%), Space: 56.8 MB (5.95%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.java
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.java
@@ -1,21 +1,19 @@
 class Solution {
-    public static int maxCoins(int[] piles) {
-
+    public  int maxCoins(int[] piles) {
         int second=0;
         Arrays.sort(piles);
         boolean checker=true;
-            for (int i = piles.length-1; piles.length/3<=i ; i--) {
-                if(checker) {
-
-                    checker=false;
-                }
-                else {
-                    second += piles[i];
-                    checker=true;
-                }
+        for (int i = piles.length-1; piles.length/3<=i ; i--) {
+            if(checker) {
+                checker=false;
             }
-
+            else {
+                second += piles[i];
+                checker=true;
+            }
+        }
         return second;
     }
+
 
 }
