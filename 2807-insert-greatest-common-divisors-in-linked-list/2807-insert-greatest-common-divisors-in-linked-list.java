7116cf5660138a580a9b7bfc8bf2e47c282c92ec
Hashem Alhariry <45319166+HashemAlhariry@users.noreply.github.com>
2023-09-24 23:45:54 +0300

    Time: 1 ms (100.00%), Space: 44 MB (40.59%) - LeetHub

--- 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.java
+++ 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.java
@@ -9,31 +9,30 @@
  * }
  */
 class Solution {
-    public  ListNode insertGreatestCommonDivisors(ListNode head) {
+   public static ListNode insertGreatestCommonDivisors(ListNode head) {
         if(head.next==null)
             return head;
         ListNode temp = head;
         ListNode nextTemp= head.next;
         while (nextTemp!=null){
-            int val = gcdByBruteForce(temp.val,nextTemp.val);
+            int val = gcdByBruteForceE(temp.val,nextTemp.val);
             ListNode node = new ListNode(val);
             temp.next=node;
             node.next=nextTemp;
-            
+
             temp=nextTemp;
             nextTemp=nextTemp.next;
         }
-        
+
         return head;
     }
-    public  int gcdByBruteForce(int n1, int n2) {
-        int gcd = 1;
-        for (int i = 1; i <= n1 && i <= n2; i++) {
-            if (n1 % i == 0 && n2 % i == 0) {
-                gcd = i;
-            }
+    private static int gcdByBruteForceE(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
         }
-        return gcd;
+        return a;
     }
 
 }
